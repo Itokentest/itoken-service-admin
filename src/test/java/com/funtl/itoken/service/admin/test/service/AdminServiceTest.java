d3e9a67afdc618ad312f0d8220ef3d8c6e7a315b
Wgw <707612123@qq.com>
2018-11-11 20:50:50 +0800

    update

---
+++ src/test/java/com/funtl/itoken/service/admin/test/service/AdminServiceTest.java
@@ -0,0 +1,31 @@
+package com.funtl.itoken.service.admin.test.service;
+
+import com.funtl.itoken.service.admin.ServiceAdminApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p>Title: AdminServiceTest</p>
+ * <p>Description: </p>
+ *
+ * @author wgw
+ * @version 1.0.0
+ * @date 2018/11/11 20:21
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceAdminApplication.class)
+public class AdminServiceTest {
+    @Test
+    public void register() {
+
+    }
+
+    @Test
+    public void login() {
+
+    }
+}
